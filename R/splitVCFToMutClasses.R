0be897b3700af0a6d4c9d55c125f211b390404e0
Anna Hakobyan <ahakobyan@cemm.oeaw.ac.at>
2020-03-05 20:46:44 +0100

    Change/fix

--- R/splitVCFToMutClasses.R
+++ R/splitVCFToMutClasses.R
@@ -1,94 +1,115 @@
 #' A function splitting vcf files into SNVs, multinucleotide substitutions and indels
 #'
 #' import GenomeInfoDb
+#' import parallel detectCores
+#' import parallel mclapply
+#' import dplyr bind_rows
+#'
 #' @examples
 #' splitVCFtoMutClasses(vcf_granges)
 #'
 #' @export
 
 
-splitVCFToMutClasses = function(vcffile) {
-  
-  snames <- names(vcffile)
-  
-  indels <- list()
-  SNVs <- list()
-  multisubs <- list()
-  
-  for(sname in snames) {
-    cat(sname, "\n")
+splitVCFToMutClasses = function(vcffile, n_cores) {
+    
+    snames <- names(vcffile)
+    
+    indels <- list()
+    SNVs <- list()
+    multisubs <- list()
+    
 
-    ## Removing non-canonical chromsomes in sample variants
-    svars <- unique(vcffile[[sname]])
-    svars <- keepStandardChromosomes(svars, pruning.mode = "tidy")
-    svars <- keepSeqlevels(svars,
-                                         value = seqlevelsInUse(svars))
+    if (missing(n_cores))
+    {
+      n_cores = detectCores()
+        if (!(.Platform$OS.type == "windows" || is.na(n_cores)))
+            n_cores <- ceiling(detectCores() * 0.75)
+        else
+          n_cores = 1
+    }
     
-    ## Removing multiallelic sites
-    
-    alt_len  <-  sapply(svars$ALT, function(x) { 
-        if (length(x) > 1) {
-            return(0)
-        } else {
-            return ( nchar( x[[1]] ) )
-        } } 
-        )
-    
-    svars  <-  svars[alt_len > 0, ]
-    alt_len  <-  alt_len [alt_len > 0 ]
-    
-    
-    svars$ALT  <-  as.character(svars$ALT@unlistData)
-    
-    indels[[sname]]  <-  svars[ alt_len != nchar(svars$REF), ]
-    
-    ## multinucleotide substitutions 
-  
-    variant_names  <-  sub( "(.*)_.*", "\\1", names(svars) )
+    for(sname in snames) {
+        cat(sname, "\n")
+
+        ## Removing non-canonical chromsomes in sample variants
+        svars <- unique(vcffile[[sname]])
+        svars <- keepStandardChromosomes(svars, pruning.mode = "tidy")
+        svars <- keepSeqlevels(svars,
+                               value = seqlevelsInUse(svars))
         
-    ## exlcude sites with the same starting coordinate
-    invalid_sites  <-  variant_names[which(duplicated(variant_names)) ]
-
-    svar_nonindels  <-  svars[ (! variant_names %in% invalid_sites) & 
-                               (nchar(svars$REF) == 1 & alt_len == 1), ]
-    
-    chr_split  <-  split(svar_nonindels, seqnames(svar_nonindels) )
-    
-    multisub_units  <-  do.call( 
-        rbind, lapply(chr_split, function(chr_ranges) {
-            chrom_name = seqlevelsInUse(chr_ranges)
-            chr_ranges = GenomicRanges::as.data.frame(chr_ranges)
-            
-            
-            ## consecutive positions list
-            cons_poss_list  <-  split(chr_ranges$start, f = cumsum(c (0, diff(chr_ranges$start) > 1)) )
-            multinucl_groups  <-  cons_poss_list[ vapply(cons_poss_list,
-                                                         length, numeric(1)) > 1 ]
-            
-            do.call(rbind, lapply ( multinucl_groups, function(x) { 
-                df_rr  <-  chr_ranges[ paste0(chrom_name, ":", x, "_"), ]
-                
-                ret_df = data.frame(
-                    seqnames = unique(df_rr$seqnames), 
-                    start = min(df_rr$start),
-                    end = max(df_rr$end),
-                    width = nrow(df_rr),
-                    strand = "*",
-                    paramRangeID = unique(df_rr$paramRangeID),
-                    REF = paste0(df_rr$REF, collapse = ""),
-                    ALT = paste0(df_rr$ALT, collapse = ""),
-                    QUAL = min(df_rr$QUAL),
-                    FILTER = ".")
-                return(df_rr)
-            } ) 
+        ## Removing multiallelic sites
+        
+        alt_len  <-  sapply (svars$ALT, function(x) { 
+            if (length(x) > 1) {
+                return(0)
+            } else {
+                return ( nchar( x[[1]] ) )
+            } } 
             )
-        } ) )
+
+        
+        svars  <-  svars[alt_len > 0, ]
+        alt_len  <-  alt_len [alt_len > 0 ]
+        
+        if ( is (svars$ALT, "DNAStringSetList")) {
+            svars$ALT  <-  as.character(svars$ALT@unlistData)
+        }
+        
+        indels[[sname]]  <-  svars[ alt_len != nchar(svars$REF), ]
+        
+        ## multinucleotide substitutions 
+        
+        variant_names  <-  sub( "(.*)_.*", "\\1", names(svars) )
+        
+        ## exlcude sites with the same starting coordinate
+        invalid_sites  <-  variant_names[which(duplicated(variant_names)) ]
+
+        svar_nonindels  <-  svars[ (! variant_names %in% invalid_sites) & 
+                                   (nchar(svars$REF) == 1 & alt_len == 1), ]
+        
+        chr_split  <-  split(svar_nonindels, seqnames(svar_nonindels) )
+
+        chr_wise_multisubs = mclapply(chr_split, function(chr_ranges) {
+                chrom_name = seqlevelsInUse(chr_ranges)
+                chr_ranges = GenomicRanges::as.data.frame(chr_ranges)
+                
+                ## consecutive positions list
+                cons_poss_list  <-  split(chr_ranges$start, f = cumsum(c (0, diff(chr_ranges$start) > 1)) )
+                multinucl_groups  <-  cons_poss_list [
+                    vapply(cons_poss_list,length, numeric(1) )  > 1
+                ]
+
+                concat_variants_df  <-  lapply (multinucl_groups, function(x) { 
+                    df_rr  <-  chr_ranges[ paste0(chrom_name, ":", x, "_"), ]
+                    
+                    ret_df = data.frame(
+                        seqnames = unique(df_rr$seqnames), 
+                        start = min(df_rr$start),
+                        end = max(df_rr$end),
+                        width = nrow(df_rr),
+                        strand = "*",
+                        paramRangeID = unique(df_rr$paramRangeID),
+                        REF = paste0(df_rr$REF, collapse = ""),
+                        ALT = paste0(df_rr$ALT, collapse = ""),
+                        QUAL = min(df_rr$QUAL),
+                        FILTER = ".")
+                    return(ret_df)
+                } )
+
+                return(bind_rows(concat_variants_df, .id = NULL))
+
+            }, mc.cores = n_cores)
+        
+        multisub_units  <-  bind_rows(chr_wise_multisubs, .id = NULL )
+        
+        multisubs[[sname]]  <-  GRanges(multisub_units)
+        SNVs[[sname]]  <-  subsetByOverlaps(svar_nonindels, multisubs[[sname]], invert = TRUE)
+        
+    }
+    multisubs = GRangesList(multisubs)
+    SNVs = GRangesList(SNVs)
     
-    multisubs[[sname]]  <-  GRanges(multisub_units)
-    SNVs[[sname]]  <-  subsetByOverlaps(svar_nonindels, multisubs[[sname]], invert = TRUE)
-    
-  }
     return(list(indels = indels, multisubs = multisubs, SNVs = SNVs))
 }
-
 
