999d2db63e66e59b5359c2bc8d17df3aae171e66
Anna Hakobyan <ahakobyan@cemm.oeaw.ac.at>
2020-03-08 21:13:52 +0100

    Mods and new functions

--- R/generic_functions.R
+++ R/generic_functions.R
@@ -1,9 +1,11 @@
-#' The function splits GRangesList files of samples into into GRangesList
-#' for individual chromosomes
+#' The function splits GRangesList files of samples into list of GRangesList
+#' for individual chromosomes in samples
 #'
 #' import GenomeInfoDb
 #' import parallel detectCores
 #' import parallel mclapply
+#' import GenomicFeatures getChromInfoFromUCSC
+#' import data.table
 #'
 #' 
 #' @export
@@ -21,21 +23,125 @@ split_chr <- function(vcf_granges_list, n_cores) {
             n_cores = 1
     }
     
-    out  <- mclapply(snames, function(sname) {
+    out  <- stats::setNames (
+                     
+                       mclapply(snames, function(sname) {
 
-        svars = vcf_granges_list[[sname]]
+                           svars = vcf_granges_list[[sname]]
+                           
+                           ## Checks if only standard chromosomes are included
+                           if ( ! all(seqlevels(svars) %in% standardChromosomes(svars)) ) {
+                               svars <- keepStandardChromosomes(svars, pruning.mode = "tidy")
+                               svars <- keepSeqlevels(svars,
+                                                      value = seqlevelsInUse(svars))
+                           }
+                           
+                           ## splits into chromosomes
+                           return(split(svars, seqnames(svars)))
+                       }, 
+                       mc.cores = n_cores),
+                       snames)
+                       
+
+    
+
+    return(out)
+}
+
+
+
+#' This function counts number of variants across samples and chromosomes.
+#' The function outputs a matrix with samples as rows and chromosomes as
+#' columns. Counts can be normalized for chromosome lengths with
+#' chr.norm = TRUE argument.
+#'
+#' import GenomeInfoDb
+#' import parallel detectCores
+#' import parallel mclapply
+#'
+#' 
+#' @export
+
+count_chr_variants  <- function(chr_split_grangeslist, chr.norm = TRUE) {
+
+    
+    counts_list <- lapply(chr_split_grangeslist,
+                          function(sample_chr) {
+                              sapply(sample_chr, length)
+                          })
+    counts_matrix  <- do.call(rbind, counts_list)
+
+    if (chr.norm) {
+        chroms  <- colnames(counts_matrix)
+
+        genome_assembly  <- genome(chr_split_grangeslist[[1]])[[1]]
+
+        chrom_sizes  <-  getChromInfoFromUCSC(genome_assembly)
+        chrom_sizes  <-  data.table(chrom_sizes)
+        setkey(chrom_sizes, chrom)
         
-        ## Checks if only standard chromosomes are included
-        if ( ! all(seqlevels(svars) %in% standardChromosomes(svars)) ) {
-            svars <- keepStandardChromosomes(svars, pruning.mode = "tidy")
-            svars <- keepSeqlevels(svars,
-                                   value = seqlevelsInUse(svars))
-        }
+        chrom_lengths  <-  setNames(chrom_sizes[chroms, length], chroms)
 
-        ## splits into chromosomes
-        return(split(svars, seqlevels(svars)))
-    }, 
-    mc.cores = n_cores)
+        length_factors  <- chrom_lengths / min(chrom_lengths)
 
-    return(GRangesList(out))
+        if ((ncol(counts_matrix) == length(length_factors))  &
+            all.equal(colnames(counts_matrix), names(length_factors) ) ) {
+                counts_matrix  <- counts_matrix / length_factors
+            } else {
+                stop( "The chromosome normalization may be wrong. \
+Naming may be disrupted.
+I'm not gonna do this to your research!
+Quitting... ")
+            }
+    }
+    return(counts_matrix)
+}
+
+
+#' Plot chromosome-wise counts with KOs as facets, colored conditions
+#' and grouped for clones
+#'
+#' import ggplot2
+#' 
+#' 
+#' @export
+
+
+plot_chrwise_counts  <- function(chr_wise_counts, KOs, treatment, clones) {
+
+    dt = as.data.frame(chr_wise_counts)
+
+    dt$KO  <-  KOs
+    dt$treatment  <-  treatment
+    dt$clones  <-  clones
+
+    dt.gg  <-  reshape2::melt(dt, id = c("KO", "treatment", "clones"))
+    
+    dt.gg$sample  <-  paste(dt.gg$KO, dt.gg$treatment, dt.gg$clones, sep ="_")
+
+
+    p  <-  ggplot(a.gg, aes(x = variable, y = value, color = treatment,
+                         group = sample) ) +
+        geom_line() +
+        geom_point() +
+        facet_wrap ( ~ KO) +
+        theme_bw() +
+        theme(axis.text.x = element_text(angle = 90))
+    
+    return(p)
+    
+}
+
+
+#' Get mutational matrix in trinucleotide context for individual chromsomes
+#' 
+#'
+#' 
+#' import ggplot2
+#' 
+#' 
+#' @export
+
+mut_matrix_chr = function() {
+    
 }
