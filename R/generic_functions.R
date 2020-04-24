acb1d12db45bf545dba14a4d3b29a0c86df077b3
Anna Hakobyan <ahakobyan@cemm.oeaw.ac.at>
2020-04-24 10:48:52 +0200

    plot_coda_continuous

--- R/generic_functions.R
+++ R/generic_functions.R
@@ -293,7 +293,83 @@ plot_coda_pca  <-  function(dt_list, sample_classes) {
     p = p + geom_segment(data = pca_loadings,
                          aes(x = 0, y = 0, xend = Comp.1 * 2,
                              yend = Comp.2 * 2),
-                         size = 1, 
+                         size =0.5, 
+                         arrow = arrow(length = unit(1/2, "picas") ),
+                         color = "black") + 
+        annotate("text", x = pca_loadings$Comp.1 * 2.2,
+                 y = pca_loadings$Comp.2 * 2.2, 
+                 label = rownames(pca_loadings)) +
+        theme_bw() + theme(legend.title = element_blank()) +
+        xlab(paste0("PC1", " (",round(percent_variance[1], 2), "%)" ) )  +
+        ylab(paste0("PC2", " (",round(percent_variance[2], 2), "%)") )
+    
+    return(p)
+}
+
+
+#' Plot CoDa PCA continuous coloring
+#' 
+#'
+#' 
+#' 
+#' @export
+
+
+plot_coda_pca_continuous  <-  function(dt_list, continuous, palette_name) {
+
+    if (missing(palette_name)) {
+        palette_name = "Spectral"
+    }
+    
+    if (is.list(dt_list)) { 
+        plot_material = do.call( rbind,
+                                lapply(names(dt_list),
+                                       function(x) {
+                                           df = dt_list[[x]];
+                                           rownames(df) = paste(x, rownames(df),
+                                                                sep = ":");
+                                           return(df)
+                                       })
+                                )
+
+        ##        sample_types = rep(sample_classes, vapply(dt_list, nrow, numeric(1)))
+    } else {
+        plot_material = t(dt_list)
+        ##        sample_types = sample_classes
+    }
+
+
+    
+    pca_list = pcaCoDa(plot_material)
+    outliers = outCoDa(plot_material)
+    print(outliers)
+
+
+    ## Getting proporition of variance explained
+    proportion_variance = (pca_list$eigenvalues ^ 2) / sum((pca_list$eigenvalues ^ 2))
+    percent_variance = 100 * proportion_variance
+
+    
+    rob_pca_scores = data.frame(pca_list$scores)
+    rob_pca_scores$col = continuous
+
+    rownames(rob_pca_scores) = rownames(plot_material)
+
+
+    p = ggplot(rob_pca_scores, aes(x = Comp.1, y = Comp.2) ) + 
+        geom_point(aes(colour = col), size = 2) +
+        scale_color_distiller(palette = palette_name ) # 
+#        scale_color_manual(values = sample(CEMM_COLORS_ALL, 9 ) ) # +
+    ## ggtitle(paste0("Rank:", nmf.rank))
+
+
+    pca_loadings = as.data.frame(pca_list$loadings)
+    rownames(pca_loadings) = colnames(plot_material)
+
+    p = p + geom_segment(data = pca_loadings,
+                         aes(x = 0, y = 0, xend = Comp.1 * 2,
+                             yend = Comp.2 * 2),
+                         size =0.5, 
                          arrow = arrow(length = unit(1/2, "picas") ),
                          color = "black") + 
         annotate("text", x = pca_loadings$Comp.1 * 2.2,
