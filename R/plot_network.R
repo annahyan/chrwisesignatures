e0eb5ea7681f9f01836a562c31d1f8ac7034016b
Anna Hakobyan <ahakobyan@cemm.oeaw.ac.at>
2020-08-24 13:04:17 +0200

    minor fix

--- R/plot_network.R
+++ R/plot_network.R
@@ -63,7 +63,7 @@ plot_network <- function(adjacency_matrix, min_threshold = 0.2, binary_matrix = 
             scale_edge_color_gradient2(low = rgb(0, 140, 160, maxColorValue = 255), 
                                        high = rgb(210, 50, 60, maxColorValue = 255)) + 
             scale_edge_width(range = c(0.8, 1.3), guide = FALSE) +
-            expand_limits(x = c(0, 4))
+            expand_limits(x = c(0, 2))
         
             }
     P = P + geom_node_point() + 
