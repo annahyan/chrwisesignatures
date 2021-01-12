0611a38880bf2668d84af60c6c201942b6f44735
Anna Hakobyan <ahakobyan@cemm.oeaw.ac.at>
2021-01-12 14:31:27 +0100

    Calculate MI among the correlations

--- R/cor_coda.R
+++ R/cor_coda.R
@@ -13,16 +13,27 @@
 #'
 #' @return Signature correlation matrix
 #'
+#' @import mpmi
+#' 
 #' @export
 
 
-cor_coda = function(x, p.val = 0.05, p.adjust = TRUE,  ...) {
+cor_coda = function(x, p.val = 0.05, p.adjust = TRUE, mi,  ...) {
+
+
+    if ( ! missing(mi) ) {
+        mi = TRUE
+    }
+    
     if (!is.matrix(x) & !is.data.frame(x)) 
         stop("x must be a matrix or data.frame")
+    
     if (any(x[!is.na(x)] <= 0)) 
         stop("all elements of x must be greater than 0")
+    
     if (ncol(x) <= 2) 
         stop("calculation of average symmetric coordinates not possible")
+    
     balZav <- function(x) {
         D <- ncol(x)
         Z.av <- matrix(NA, ncol = 2, nrow = nrow(x))
@@ -38,6 +49,7 @@ cor_coda = function(x, p.val = 0.05, p.adjust = TRUE,  ...) {
         p4 <- 1/(D - 1 + sqrt(D * (D - 2)))
         Z.av[, 1] <- p1 * (log(x[, 1]/(x[, 2]^p4 * p2^p3)))
         Z.av[, 2] <- p1 * (log(x[, 2]/(x[, 1]^p4 * p2^p3)))
+
         return(Z.av)
     }
 
@@ -51,10 +63,22 @@ cor_coda = function(x, p.val = 0.05, p.adjust = TRUE,  ...) {
 
             balZavout = balZavout[is.finite(rowSums(balZavout)), ]
 
-            corout = cor.test(balZavout[,1], balZavout[, 2], ...)
-            corPvals[i, j]  <-  corout$p.value
 
-            corZav[i, j] <- corout$estimate
+            if (mi == TRUE) { ### check if the mpmi MI calculation should be used
+
+                mi.out = mpmi::cmi(balZavout[,1], balZavout[, 2])
+
+                corZav[i, j] <- mi.out$bcmi
+                corPvals[i, j] <- 2 * ( -abs( mi.out$zscore ))
+                
+            } else { ### cor.test is used instead
+                
+                
+                corout = cor.test(balZavout[,1], balZavout[, 2], ...)
+                corPvals[i, j]  <-  corout$p.value
+
+                corZav[i, j] <- corout$estimate
+            }
         }
     }
 
