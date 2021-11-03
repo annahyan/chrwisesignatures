265ee7176411de8cbb97486d7db7a834bf1a9b55
annahyan <anna@menchelab.com>
2021-11-03 19:52:59 +0100

    moved balZav functions to a separate file

---
+++ R/balZav_funcs.R
@@ -0,0 +1,36 @@
+balZav <- function(x) {
+    D <- ncol(x)
+    Z.av <- matrix(NA, ncol = 2, nrow = nrow(x))
+    p1 <- sqrt(D - 1 + sqrt(D * (D - 2)))/sqrt(2 * D)
+    if (D == 3) {
+        p2 <- x[, 3]
+    }
+    else {
+        p2 <- apply(x[, 3:D], 1, prod)
+    }
+    p3 <- (sqrt(D - 2) + sqrt(D))/(sqrt(D - 2) * (D - 1 + 
+                                                  sqrt(D * (D - 2))))
+    p4 <- 1/(D - 1 + sqrt(D * (D - 2)))
+    Z.av[, 1] <- p1 * (log(x[, 1]/(x[, 2]^p4 * p2^p3)))
+    Z.av[, 2] <- p1 * (log(x[, 2]/(x[, 1]^p4 * p2^p3)))
+
+    return(Z.av)
+}
+
+balZavRow <- function(x) {
+    D <- length(x)
+    Z.av <- c(0,0)
+    p1 <- sqrt(D - 1 + sqrt(D * (D - 2)))/sqrt(2 * D)
+    if (D == 3) {
+        p2 <- x[3]
+    }
+    else {
+        p2 <- prod(x[3:D])
+    }
+    p3 <- (sqrt(D - 2) + sqrt(D))/(sqrt(D - 2) * (D - 1 + 
+                                                  sqrt(D * (D - 2))))
+    p4 <- 1/(D - 1 + sqrt(D * (D - 2)))
+    Z.av[1] <- p1 * (log(x[1]/(x[2]^p4 * p2^p3)))
+    Z.av[2] <- p1 * (log(x[2]/(x[1]^p4 * p2^p3)))
+    return(Z.av)
+}
--- R/symmetric_pivot_coords.R
+++ R/symmetric_pivot_coords.R
@@ -28,25 +28,7 @@ symmetric_pivot_coords = function(x, i, j, rand.add = FALSE,  ...) {
     if (ncol(x) <= 2) 
         stop("calculation of average symmetric coordinates not possible")
     
-    balZav <- function(x) {
-        D <- ncol(x)
-        Z.av <- matrix(NA, ncol = 2, nrow = nrow(x))
-        p1 <- sqrt(D - 1 + sqrt(D * (D - 2)))/sqrt(2 * D)
-        if (D == 3) {
-            p2 <- x[, 3]
-        }
-        else {
-            p2 <- apply(x[, 3:D], 1, prod)
-        }
-        p3 <- (sqrt(D - 2) + sqrt(D))/(sqrt(D - 2) * (D - 1 + 
-            sqrt(D * (D - 2))))
-        p4 <- 1/(D - 1 + sqrt(D * (D - 2)))
-        Z.av[, 1] <- p1 * (log(x[, 1]/(x[, 2]^p4 * p2^p3)))
-        Z.av[, 2] <- p1 * (log(x[, 2]/(x[, 1]^p4 * p2^p3)))
-
-        return(Z.av)
-    }
-
+    
     ind <- c(1:ncol(x))
     rearranged.x = x[, c(i, j, ind[-c(i, j)])]
     if (rand.add) {
