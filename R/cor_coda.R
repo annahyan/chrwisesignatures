8e675941ae7b6f431fd3fedf74bfd98dbd58f64c
annahyan <anna@menchelab.com>
2021-11-05 15:17:22 +0100

    minor fix

--- R/cor_coda.R
+++ R/cor_coda.R
@@ -77,8 +77,14 @@ cor_coda = function(x,  p.val = 0.05, rand.add = FALSE,
                                       if (length(rowvec) == 2) return(c(0,0))
                                       return(balZavRow(rowvec))
                                   } ) %>% t()
-                balZavout = balZavout[rowSums(abs(balZavout)) > 0, ]
                 
+                balZavout = balZavout[rowSums(abs(balZavout)) > 0, , drop = FALSE]
+
+                if (nrow(balZavout) < 3 ) {
+                    corPvals[i, j]  <-  1
+                    corZav[i, j] <- 0
+                    next
+                }
             }
 
 
